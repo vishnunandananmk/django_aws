d4ac4d582900bfd8a6ae4bd7794cb26093bda44f
root <root@ip-172-31-5-144.ap-south-1.compute.internal>
2024-08-31 08:35:10 +0000

    initial commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-g2uy-p5^vma0we)q@sf)_c-oyx=89y8^4m2y*%b+n)xe1hqt!("
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
